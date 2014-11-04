77ca37bfbae19fc454218b9f217fdfbf261b05bc
Ian Quick <ian.quick@gmail.com>
2014-11-03 19:08:29 -0500

    0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject clj-ejson "0.1.0-SNAPSHOT"
+(defproject clj-ejson "0.1.0"
   :description "Clojure version of https://github.com/Shopify/ejson"
   :url "https://github.com/ibawt/clj-ejson"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
