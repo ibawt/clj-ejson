64ba2677baf66da8e5b2fab61f1ddaf0750c21de
Ian Quick <ian.quick@gmail.com>
2014-11-03 19:12:17 -0500

    remove plugin

--- project.clj
+++ project.clj
@@ -7,5 +7,4 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.bouncycastle/bcprov-jdk15on "1.51"]
                  [org.bouncycastle/bcpkix-jdk15on "1.51"]
-                 [cheshire "5.3.1"]]
-  :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]])
+                 [cheshire "5.3.1"]])
